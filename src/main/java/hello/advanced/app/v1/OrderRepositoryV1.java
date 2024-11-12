7fbb842af016a0a6c74635451683d2d839e96f22
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-11-12 23:38:29 +0900

    인프런 로그추적기 v1 개발 - service, repository 추가

--- src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,13 +9,26 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
+    private final HelloTraceV1 trace;
+
     public void save(String itemId){
-        //저장 로직
-        if(itemId.equals("ex")){
-            throw new IllegalStateException("예외발생!!");
+
+        TraceStatus status=null;
+        try {
+            status= trace.begin("OrderRepositoryV1.save()"); //수작업으로 해줘야함
+            //저장 로직
+            if(itemId.equals("ex")){
+                throw new IllegalStateException("예외발생!!");
+            }
+
+            sleep(1000);
+            trace.end(status);
+        }catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
         }
 
-        sleep(1000);
+
     }
 
     private void sleep(long millis){
--- src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,19 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV1 {
 
     private final OrderRepositoryV1 orderRepositoryV1;
+    private final HelloTraceV1 trace;
 
     public void orderItem(String itemId){
-        orderRepositoryV1.save(itemId);
+        TraceStatus status=null;
+        try {
+            status= trace.begin("OrderServiceV1.orderItem()"); //수작업으로 해줘야함
+            orderRepositoryV1.save(itemId);
+            //예외가 터져도 end가 출력되어야 한다 -> try catch
+            trace.end(status);
+        }catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+
     }
 }
