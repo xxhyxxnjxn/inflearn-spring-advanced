85b6a90f01da2abd454d2f600caea775f53fc7be
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-11-17 13:39:55 +0900

    인프런 로그추적기 개발 - 필드 동기화 코드에 적용하기

---
+++ src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,33 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.FieldLogTrace;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderServiceV3;
+    private final FieldLogTrace trace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId){
+        TraceStatus status=null;
+        try {
+            status= trace.begin("OrderControllerV1.request()"); //수작업으로 해줘야함
+            orderServiceV3.orderItem(itemId);
+            //예외가 터져도 end가 출력되어야 한다 -> try catch
+            trace.end(status);
+            return "ok";
+        }catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
---
+++ src/main/java/hello/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,31 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.FieldLogTrace;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepositoryV3;
+    private final FieldLogTrace trace;
+
+    public void orderItem(String itemId){
+        TraceStatus status=null;
+        try {
+            status= trace.begin("OrderServiceV1.orderItem()"); //수작업으로 해줘야함
+            orderRepositoryV3.save(itemId);
+            //예외가 터져도 end가 출력되어야 한다 -> try catch
+            trace.end(status);
+        }catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+
+    }
+}
